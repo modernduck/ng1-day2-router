36d825c8828dd2d5112ed8c686bb471b1a5a4a3b
Sompop Kulapalanont <sompopkulapalanont@Sompops-MacBook-Pro.local>
2017-05-24 12:44:42 +0700

    basic routing'

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-  angular.module('myApp', [])
+  angular.module('myApp', ['ngRoute'])
+    .config( function($routeProvider){
+        //going to config which route to go where
+        $routeProvider.when("/", {
+            templateUrl:"pages/main.html"
+        })
+        .when("/london", {
+            templateUrl:"pages/london.html"
+        })
+
+
+    })
